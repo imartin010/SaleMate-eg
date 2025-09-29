ecc607d49abca8cd911030b73f349eb5c5067a8c
imartin010 <themartining@gmail.com>
2025-09-29 15:00:28 +0300

    Deploy: 2025-09-29 15:00:28 - Clean up SQL files and add technical report

--- check_projects_schema.sql
+++
@@ -1,9 +0,0 @@
--- Check the actual schema of the projects table
-SELECT 
-    column_name,
-    data_type,
-    is_nullable
-FROM information_schema.columns 
-WHERE table_name = 'projects' 
-AND table_schema = 'public'
-ORDER BY ordinal_position;
